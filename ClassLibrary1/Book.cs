d66a73b734ec433bfa20e7f2bb9920a7f7706214
Benito Martiarena <benito.martiarena@dataart.com>
2023-02-26 16:07:57 -0300

    Initial solution. Adding domain class.

---
+++ ClassLibrary1/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Editorial { get; set; }
+        public BookStatus Status { get; set; }
+
+        public Book(string name, string editorial)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Editorial = editorial;
+            Status = BookStatus.Available;
+        }
+    }
+}
